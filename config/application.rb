1fc2fe1ed61cb18b992699c137bee0f4a103a264
dpoit <dannypoit@gmail.com>
2017-05-30 20:20:21 -0400

    Setup custom fonts per Lesson 10.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Splurty
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .ttf .woff )
+
   end
 end
+
+
